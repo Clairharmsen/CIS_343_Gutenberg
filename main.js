9c436fc00daba919dc07380e0718cc0095421611
Clair <harmsesi@mail.gvsu.edu>
2025-03-28 10:59:55 -0400

    added starter code

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 //Clair Harmsen
 //Ezekiel Turnbough
 // 3/28/2025
+
+const url = 'https://gutendex.com/books?search=';
+ 
+async function getData(str){
+  const request = await fetch(url + str);
+  const json = await request.json();
+  for(let i=0; i<json.count; ++i){
+    console.log(json.results[i].title);
+  }
+}
+ 
+getData("sherlock");
